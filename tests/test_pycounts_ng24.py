c7ed0d80f51bd782bcb81399f8e64fdc509ceac5
nassimgha <150398381+nassimgha@users.noreply.github.com>
2024-01-29 15:25:18 -0800

    Update test_pycounts_ng24.py

--- tests/test_pycounts_ng24.py
+++ tests/test_pycounts_ng24.py
@@ -1,5 +1,5 @@
 from pycounts_ng24 import pycounts_ng24
-from pycounts.pycounts_ng24 import count_words
+from pycounts_ng24.pycounts_ng24 import count_words
 from collections import Counter
 
 def test_count_words():
